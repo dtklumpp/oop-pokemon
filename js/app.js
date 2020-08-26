f34380f28d352e8b3e22f1f0588c8bda6f18d388
dtklumpp <dtklumpp@gmail.com>
2020-08-26 12:57:01 -0400

    fixed bugs game running now working on UI and such

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@ const gamePoke = {
     cardList: pokeCards,
     //cardsLeft: [],
     round: 1,
-    points_per_round: 1,
+    points_per_round: 3,
     playedCards: [],
     rounds_per_game: 3,
 
@@ -96,15 +96,19 @@ const gamePoke = {
     pickCard() {
         logit('pick card marker');
         index1 = Math.floor(this.cardsLeft.length * Math.random());
-        logit(index1);
-        logit(this.cardsLeft[index1]);
-        let card1 = this.cardsLeft.splice(index1,1);
-        logit(card1.name);
+        //logit(index1);
+        //logit(this.cardsLeft[index1].name);
+        //logit(this.cardsLeft.length);
+        //logit(this.cardsLeft.splice(index1,1));
+        let card1 = this.cardsLeft.splice(index1,1)[0];
+        //logit(this.cardsLeft.length);
+        //logit(card1.name);
         this.playedCards.push(card1);
         return card1;
     },
 
     dealCards(player) {
+        logit('deal cards marker');
         player.hand.splice(0,player.hand.length-1); //clear hand
         player.hand.push(this.pickCard());
         player.hand.push(this.pickCard());
@@ -112,31 +116,40 @@ const gamePoke = {
     },
 
     playPoint(human, computer) {
-        logit(human.hand);
+        logit('play point marker');
+        //logit(human.hand);
         //humChoice = prompt("which card?");
         humChoice = Math.floor(Math.random() * human.hand.length);
-        humCard = human.hand.splice(humChoice-1,1);
+        humCard = human.hand.splice(humChoice-1,1)[0];
         compChoice = Math.floor(Math.random() * computer.hand.length);
-        compCard = computer.hand.splice(compChoice, 1);
-        human.history.push(humCard);
-        computer.history.push(compCard);
-        logit(humCard.name);
-        logit(compCard.name);
+        compCard = computer.hand.splice(compChoice, 1)[0];
+        human.cardHistory.push(humCard);
+        computer.cardHistory.push(compCard);
+        logit("human plays "+humCard.name+" for "+humCard.damage);
+        logit("computer plays "+compCard.name+" for "+compCard.damage);
         if(humCard.damage > compCard.damage){
             human.score += 1;
         }
         else if (compCard.damage > humCard.damage){
             computer.score += 1;
         }
+        logit(human.score);
+        logit(computer.score);
     },
 
     playRound(human, computer) {
+        logit("playround marker");
         this.cardsLeft = this.cardList;
         this.dealCards(human);
         this.dealCards(computer);
-        logit("playround marker");
-        logit(human.hand[1].name);
-        logit(computer.hand[1].name);
+        logit("human hand:");
+        for(let i = 0; i < 3; i++){
+          logit(human.hand[i].name);
+        }
+        logit("computer hand:");
+        for(let i = 0; i < 3; i++){
+          logit(computer.hand[i].name);
+        }
         //reset score
         human.score = 0;
         computer.score = 0;
@@ -146,29 +159,35 @@ const gamePoke = {
         }
         if(human.score > computer.score){
             human.rounds += 1;
+            logit('human wins round!');
         }
         else if(computer.score > human.score){
             computer.rounds += 1;
+            logit('computer wins round!')
         }
+        else {logit('nobody wins round!');}
+        logit("human rounds won = "+human.rounds);
+        logit("computer rounds won = "+computer.rounds);
     },
 
 
 
 
     playPoke(human,computer) {
+        logit('play game marker');
         for(let i = 1; i <= this.rounds_per_game; i++){
             logit("round "+i+" begin!");
             this.playRound(human, computer);
         }
         //human wins ties
         if(human.rounds >= computer.rounds){
-            console.log("Human Wins!");
+            console.log("Human Wins Game!");
         }
-        else {console.log("Computer Wins!")}
-        logit(human.history);
-        logit(computer.history);
-        logit(human.round);
-        logit(computer.round);
+        else {console.log("Computer Wins Game!")}
+        logit(human.cardHistory);
+        logit(computer.cardHistory);
+        logit(human.rounds);
+        logit(computer.rounds);
         logit(human.score);
         logit(computer.score);
     }
@@ -182,12 +201,13 @@ class Player {
         this.rounds = 0;
         this.hand = [];
         this.score = 0;
-        this.history = [];
+        this.cardHistory = [];
         this.humanity = isHuman;
     }
     chooseCard() {
         //do this later...
     }
+    //in fact move all doubled methods above in here...
 }
 
 const david = new Player(true);
